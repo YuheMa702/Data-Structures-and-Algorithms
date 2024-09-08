cdfa12314c027e48dd963804a6087ba148bf8192
Yuhe Ma <mayuhe2002doubleflowers@gmail.com>
2024-09-07 23:43:52 -0400

    Update dp problems

--- Algorithms/dp.py
+++ Algorithms/dp.py
@@ -37,3 +37,24 @@ def length_of_LIS(nums):
                 dp[i] = max(dp[i], dp[j] + 1)
     
     return max(dp)
+
+
+def knapsack(values, weights, W):
+    n = len(values)
+    dp = [[0 for _ in range(W + 1)] for _ in range(n + 1)]
+    
+    for i in range(1, n + 1):
+        for w in range(1, W + 1):
+            if weights[i - 1] <= w:
+                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1])
+            else:
+                dp[i][w] = dp[i - 1][w]
+                
+    return dp[n][W]
+
+# Example usage:
+values = [60, 100, 120]
+weights = [10, 20, 30]
+W = 50
+print(knapsack(values, weights, W))  # Output: 220
+
---
+++ crash.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void winner() {
+    printf("You Win!");
+}
+
+
+int main() {
+    winner();
+    return 0;
+}
